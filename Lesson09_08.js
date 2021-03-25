677ae1d8113d42dfd80af34d90aa9349263e2851
corogit <66649016+corogit@users.noreply.github.com>
2021-03-25 23:01:23 +0900

    [演算子]

---
+++ Lesson09_08.js
@@ -0,0 +1,12 @@
+// 複合代入演算子
+var x = 8;
+var y = 12;
+var z = 20;
+
+x += 10;
+// x = x + 10
+z += y;
+// z = z + y
+
+console.log(x);
+console.log(z);
---
+++ Lesson09_09.js
@@ -0,0 +1,11 @@
+// インクリメント (incriment)
+// デクリメント (decriment)
+
+var x = 8;
+var y = 8;
+
+x++;
+y--;
+
+console.log(x);
+console.log(y);
