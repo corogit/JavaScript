2f5b3b5bd86a1d8665f9bd9fcbbe56b0d245c9e6
corogit <66649016+corogit@users.noreply.github.com>
2021-03-26 22:01:23 +0900

    [条件分岐]

---
+++ Lesson10_check.js
@@ -0,0 +1,15 @@
+var age = 10;
+var age = 21;
+var age = 32;
+var age = 43;
+var age = 5;
+
+if  (age >= 10 && age < 20) {
+  console.log("10代");
+} else if (age >= 20 && age < 30) {
+  console.log("20代");
+} else if (age >= 30 && age < 40) {
+  console.log("30代");
+} else {
+  console.log("それ以外");
+}
